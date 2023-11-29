d830c3ba57f44106384bbbf378a731ae50156a40
KHARERISHABH <rishabkhare41@gmail.com>
2023-11-29 15:36:16 +0530

    creating component with the CLI and Nested Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent
+    AppComponent, ServerComponent, ServersComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule
