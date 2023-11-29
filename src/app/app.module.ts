499f668884368653a4a0f195ce6992f23d86713d
KHARERISHABH <rishabkhare41@gmail.com>
2023-11-29 16:30:36 +0530

    Assigment 1 Making 2 Component warning and success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ServerComponent, ServersComponent
+    AppComponent, ServerComponent, ServersComponent, WarningComponent, SuccessComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule
